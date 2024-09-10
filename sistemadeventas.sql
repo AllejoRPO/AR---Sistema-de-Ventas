c2b5f4ec4b36a6373ee7a59638dfef41099c483e
AllejoRPO <alejandrorpo198920@gmail.com>
2024-09-10 14:32:36 -0500

    Versión 9

--- sistemadeventas.sql
+++ sistemadeventas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3307
--- Tiempo de generación: 10-09-2024 a las 18:29:50
+-- Tiempo de generación: 10-09-2024 a las 21:31:21
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
